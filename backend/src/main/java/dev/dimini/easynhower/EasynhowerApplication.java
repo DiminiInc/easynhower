ff9570d37c17015a6b7474de1e7caad16597cf84
Dmitry Yaskovich <kadavr95@outlook.com>
2026-09-08 23:57:23 +0300

    Initial commit

---
+++ backend/src/main/java/dev/dimini/easynhower/EasynhowerApplication.java
@@ -0,0 +1,13 @@
+package dev.dimini.easynhower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasynhowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasynhowerApplication.class, args);
+	}
+
+}
